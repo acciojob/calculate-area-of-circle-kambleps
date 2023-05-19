be432cf01f34baba61b14408f9a9f4977ae4b228
kambleps <125243027+kambleps@users.noreply.github.com>
2023-05-19 18:50:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+let radius = prompt("Enter the radius");
+let area = radius*radius*3.142;
+let round = Math.round(area*100)/100;
+console.log("The area of the circle with radius "+radius+" is "+round);
